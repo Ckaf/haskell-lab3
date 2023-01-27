d827a62cf6b14a808667717fed21a43a90c442ee
Ckaf <borisnovoselov602@gmail.com>
2023-01-27 13:20:46 +0300

    Update integration_test.py
    tests fix 2.0

--- python/test/integration_test.py
+++ python/test/integration_test.py
@@ -4,29 +4,29 @@ import os
 
 
 def test_1():
-    stream = os.popen('cat ./data/points.txt | haskell-lab3-exe --left 1 --right 10 --lm')
+    stream = os.popen('cat ./python/test/data/points.txt | haskell-lab3-exe --left 1 --right 10 --lm')
     res = stream.read()
-    file = open("./data/expected_result1.txt", "r")
+    file = open("./python/test/data/expected_result1.txt", "r")
     expected = file.read()
     assert(res == expected)
 
 def test_2():
-    stream = os.popen('cat ./data/points.txt | haskell-lab3-exe --left 1 --right 10 --sm')
+    stream = os.popen('cat ./python/test/data/points.txt | haskell-lab3-exe --left 1 --right 10 --sm')
     res = stream.read()
-    file = open("./data/expected_result2.txt", "r")
+    file = open("./python/test/data/expected_result2.txt", "r")
     expected = file.read()
     assert(res == expected)
 
 def test_3():
-    stream = os.popen('cat ./data/points.txt | haskell-lab3-exe --left 1 --right 10 --lm --sm')
+    stream = os.popen('cat ./python/test/data/points.txt | haskell-lab3-exe --left 1 --right 10 --lm --sm')
     res = stream.read()
-    file = open("./data/expected_result3.txt", "r")
+    file = open("./python/test/data/expected_result3.txt", "r")
     expected = file.read()
     assert(res == expected)
 
 def test_4():
     stream = os.popen('haskell-lab3-exe --left 1 --right 10 --sm --file \"./data/points\"')
     res = stream.read()
-    file = open("./data/expected_result4.txt", "r")
+    file = open("./python/test/data/expected_result4.txt", "r")
     expected = file.read()
     assert(res == expected)
