9f48c898457e38ea14484df3160a7589a4d9066d
ckafff <borisnovoselov602@gmail.com>
2023-02-06 23:21:25 +0300

    fix python tests

--- python/test/integration_test.py
+++ python/test/integration_test.py
@@ -1,9 +1,11 @@
 
 import pytest
 import os
+
 def path_to_bin():
-    stream = os.popen('cabal exec which haskell-lab3-exe')
+    stream = os.popen('stack exec which haskell-lab3-exe')
     bin_p = stream.read()
+    bin_p = bin_p.replace('\n', '')
     return bin_p
 
 def test_1():
@@ -14,21 +16,21 @@ def test_1():
     assert(res == expected)
 
 def test_2():
-    stream = os.popen('cat ./python/test/data/points.txt | '+ path_to_bin() +'--left 1 --right 10 --sm')
+    stream = os.popen('cat ./python/test/data/points.txt | '+ path_to_bin() +' --left 1 --right 10 --sm')
     res = stream.read()
     file = open("./python/test/data/expected_result2.txt", "r")
     expected = file.read()
     assert(res == expected)
 
 def test_3():
-    stream = os.popen('cat ./python/test/data/points.txt | '+ path_to_bin() +'--left 1 --right 10 --lm --sm')
+    stream = os.popen('cat ./python/test/data/points.txt | '+ path_to_bin() +' --left 1 --right 10 --lm --sm')
     res = stream.read()
     file = open("./python/test/data/expected_result3.txt", "r")
     expected = file.read()
     assert(res == expected)
 
 def test_4():
-    stream = os.popen( path_to_bin() + '--left 1 --right 10 --sm --file \"./python/test/data/points\"')
+    stream = os.popen( path_to_bin() + ' --left 1 --right 10 --sm --file \"./python/test/data/points.txt\"')
     res = stream.read()
     file = open("./python/test/data/expected_result4.txt", "r")
     expected = file.read()
